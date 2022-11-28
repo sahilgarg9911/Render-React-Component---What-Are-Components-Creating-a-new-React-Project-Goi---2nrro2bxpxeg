97100c19304a693c94cb07341d54cede59439ab1
Sahil Garg <sahilgarg9911@gmail.com>
2022-11-28 16:19:04 +0530

    assignmentnewton

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
+    return ( <
+        div id = "main" >
+        <
+        p > I am learning React.My life is getting better.s < /p> < /
+        div >
+    )
 }
 
 
